569b55ff25afc126e5662c28fe826cadd1f52210
Neide Urbina <neide.n.urbina@gmail.com>
2021-09-24 22:43:34 -0500

    Gitpub database setup

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ const express = require('express');
 const app = express();
 //set variable port to 3000
 const port = 3000;
+//to be required by server from database (/models/drinks.js)
+const drinks = require("./models/drinks");
 
 //home get route
 app.get('/', (req, res) => {
     res.send('Welcome to the Gitpub App!');
 });
+
+//route to display drinks as json in browser
+app.get('/drinks', (req, res) => {
+    res.send(drinks);
+})
+
 
 //setting to listen port 3000 with message
 app.listen(port, () => {
